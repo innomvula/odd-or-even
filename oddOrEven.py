30984ebb181a20f92de8ac99c40a5b3bad7a9cc4
Inno Mvula <65461268+innomvula@users.noreply.github.com>
2021-08-03 16:18:37 +0000

    cleaned up code

--- oddOrEven.py
+++ oddOrEven.py
@@ -1,19 +1,24 @@
-newnum = True
-gamecont = ["y", "n"]
-while newnum == True:
+def odd_even():
     num = int(input("what number are you thinking? "))
     if num % 2 != 0:
         print("That's an odd number! Have another?")
     else:
         print("That's an even number!!")
-    while newnum == True:
-        newattempt = input("New number (y/n)? ")
-        if newattempt in gamecont and newattempt == "n":
-            newnum = False
-        elif newattempt in gamecont and newattempt == "y":
-            break
-        else:
-            print("Wrong input!")
-            
+
+def odd_even_main():
+    playagain = True
+    gamecont = ["y", "n"]
+    while playagain == True:
+        odd_even()
+        while playagain == True:
+            newgame = input("Play again (y/n)?: ")
+            if newgame in gamecont and newgame == "n":
+                playagain = False
+            elif newgame in gamecont and newgame == "y":
+                break
+            else:
+                print("Wrong input! Enter 'y' or 'n'.")
+        print()
+#main()
     
     
